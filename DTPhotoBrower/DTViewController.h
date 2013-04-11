d7fba45ccd3fd3bb2de29fa61b3ba08ba457dcf1
darktt <pippipig@gmail.com>
2013-04-11 09:04:12 +0800

    Initial Commit

---
+++ DTPhotoBrower/DTViewController.h
@@ -0,0 +1,13 @@
+//
+//  DTViewController.h
+//  DTPhotoBrower
+//
+//  Created by Darktt on 13/4/11.
+//  Copyright (c) 2013 Darktt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTViewController : UIViewController
+
+@end
---
+++ DTPhotoBrower/DTViewController.m
@@ -0,0 +1,29 @@
+//
+//  DTViewController.m
+//  DTPhotoBrower
+//
+//  Created by Darktt on 13/4/11.
+//  Copyright (c) 2013 Darktt. All rights reserved.
+//
+
+#import "DTViewController.h"
+
+@interface DTViewController ()
+
+@end
+
+@implementation DTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
